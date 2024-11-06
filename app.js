ea658734ba4211c7645ca75921f945294f861918
VydehiR <vydehi1303@gmail.com>
2024-11-05 19:36:09 -0600

    modifies app.js

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sculptures', sculpturesRouter); // Add the route for Sculptures
+app.use('/grid', gridRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
