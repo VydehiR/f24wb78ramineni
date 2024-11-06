d204d2a7333b0e96934b9e39790055c2428c3917
VydehiR <vydehi1303@gmail.com>
2024-11-05 18:40:40 -0600

    added Sculpture.js and Sulptures.pug

--- routes/Sculptures.js
+++ routes/Sculptures.js
@@ -1,8 +1,15 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
+/* GET Sculptures page. */
+const sculptures = [
+    { name: "The Thinker", height: 182, material: "Bronze" },
+    { name: "Venus de Milo", height: 203, material: "Marble" },
+    { name: "David", height: 517, material: "Marble" }
+];
 router.get('/', (req, res) => {
-    res.render('sculptures', { title: 'Search Results - Sculptures' });
+    res.render('artifacts', {
+        title: 'Search Results - Artifacts',
+        results: artifacts
+    });
 });
-
-module.exports = router;
