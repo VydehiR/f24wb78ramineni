6f29006c1bcea9fad950b45f7e0fcd9d438f9010
VydehiR <vydehi1303@gmail.com>
2024-11-05 19:02:17 -0600

    changes Sculpture.js and Sulptures.pug

--- routes/Sculptures.js
+++ routes/Sculptures.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-// Sample data for artifacts
-const artifacts = [
-  { artifact_name: 'Ancient Vase', artifact_age: 1200, origin_culture: 'Greek' },
-  { artifact_name: 'Stone Tablet', artifact_age: 3500, origin_culture: 'Egyptian' },
-  { artifact_name: 'Samurai Sword', artifact_age: 600, origin_culture: 'Japanese' }
+// Sample data for Sculptures
+const Sculptures = [
+    { sculptures_name: "The Thinker", sculptures_height: 182, material: "Bronze" },
+    { sculptures_name: "Venus de Milo", sculptures_height : 203, material: "Marble" },
+    { sculptures_name: "David", sculptures_height: 517, material: "Marble" }
 ];
 
 router.get('/', (req, res) => {
   res.render('artifacts', { 
-    title: 'Search Results - Artifacts', 
-    results: artifacts 
+    title: 'Search Results - Sculptures', 
+    results: Sculptures 
   });
 });
 
